bd3f1c02de5e99e193cf4f7b5c38ebdebee18dfe
Ubuntu <patrick@ls32v2ubuntu.abvhcs0wu0eula5yzszgyczaoe.ax.internal.cloudapp.net>
2019-01-08 09:20:11 +0000

    Add script to run all queries

---
+++ clickhouse/run-all.sh
@@ -0,0 +1,10 @@
+clickhouse-client < 1-most-pop.sql
+clickhouse-client < 2-most-pop.sql
+clickhouse-client < 3-cities-departure.sql
+clickhouse-client < 4-flight-delay.sql
+clickhouse-client < 5-cities-departure.sql
+clickhouse-client < 6-flight-max-duration.sql
+clickhouse-client < 7-distribution-arrival.sql
+clickhouse-client < 8-air-companies-stop.sql
+clickhouse-client < 9-most-trending-dest.sql
+clickhouse-client < A-most-popularity-cities.sql
