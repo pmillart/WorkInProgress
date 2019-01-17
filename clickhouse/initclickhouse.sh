4123c4882eb061e83a0b4eab1b4d7fc790fd9dac
Ubuntu <patrick@ls32v2ubuntu.abvhcs0wu0eula5yzszgyczaoe.ax.internal.cloudapp.net>
2019-01-17 12:36:58 +0000

    duplicate entries in fstab

--- clickhouse/initclickhouse.sh
+++ clickhouse/initclickhouse.sh
@@ -1,15 +1,16 @@
 export NVMe="/dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1"
-export CLICKHOUSEDATA="/home/patrick/clickhouse"
+export CLICKHOUSEDATA="/datadisk/clickhouse"
 export CLICKHOUSELABEL="clickhouse-data"
 
 
 echo "y"|mdadm --create /dev/md0 -f --level=0 --assume-clean --raid-devices=4 $NVMe
 mdadm --monitor --daemonise /dev/md0
-mkfs.ext4 -F /dev/md0
+mkfs.ext4 -G 4096 -F /dev/md0
 e2label /dev/md0 $CLICKHOUSELABEL
 
 mkdir -p $CLICKHOUSEDATA
-echo "LABEL=$CLICKHOUSELABEL $CLICKHOUSEDATA ext4 rw,relatime,nobarrier,nofail,stripe=512,data=ordered 0 0" >> /etc/fstab
+#Adding nofail: Si changemement de machine, les NVMe sont détruits et peuvent empecher de démarrer lle server
+grep $CLICKHOUSELABEL /etc/fstab || echo "LABEL=$CLICKHOUSELABEL $CLICKHOUSEDATA ext4 rw,noatime,discard,nobarrier,nofail,data=ordered 0 0" >> /etc/fstab
 mount  $CLICKHOUSEDATA
 
 chown clickhouse:clickhouse $CLICKHOUSEDATA
