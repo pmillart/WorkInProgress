bce5acc4278e0e658f0f3e81d5119cc041e92240
Patrick Millart <pamillar@microsoft.com>
2019-02-04 18:05:58 +0100

    Add tfstat to backend

---
+++ Terraform/env.sh
@@ -0,0 +1,1 @@
+export ARM_ACCESS_KEY=$(cat ~/.ssh/pmshare.accesskey)
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,4 +5,10 @@ provider "azurerm" {
     partner_id = "f18c316d-fd02-4fe4-a115-1efc30e3c8fb"
 }
 
-
+terraform {
+  backend "azurerm" {
+    storage_account_name = "pmtshare"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
